8c19c3076e7f8716ed5178286634153d71a5d653
rafavicruz <rafaelvicruz@gmail.com>
2026-03-25 15:52:02 -0300

    login teste

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
-  // Assim que a página carregar, o usuário será enviado para index.blog
-        window.onload = function() {
-            window.location.href = "index.blog";
-        };
+document.addEventListener('DOMContentLoaded', function() {
+  const redirectToIndexBlog = function() {
+    window.location.href = 'index.blog';
+  };
+
+  const loginButton = document.getElementById('loginButton');
+  const registerButton = document.getElementById('registerButton');
+
+  if (loginButton) {
+    loginButton.addEventListener('click', redirectToIndexBlog);
+  }
+
+  if (registerButton) {
+    registerButton.addEventListener('click', redirectToIndexBlog);
+  }
+});
