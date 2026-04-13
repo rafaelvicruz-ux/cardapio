bec197eabc14dc37a6d2a658ce3c8f7b9485a3e8
rafavicruz <rafaelvicruz@gmail.com>
2026-04-13 17:52:10 -0300

    tentando apagar login

--- data/schema.sql
+++ data/schema.sql
@@ -7,21 +7,3 @@ CREATE TABLE users (
   password TEXT NOT NULL,
   token TEXT NULL
 );
-
-CREATE TABLE groceries (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  userId INTEGER NOT NULL,
-  item TEXT NOT NULL,
-  period TEXT NOT NULL CHECK (period IN ('week', 'month')),
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (userId) REFERENCES users(id)
-);
-
-CREATE TABLE mealplans (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  userId INTEGER NOT NULL,
-  recipe TEXT NOT NULL,
-  period TEXT NOT NULL CHECK (period IN ('week', 'month')),
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (userId) REFERENCES users(id)
-);
