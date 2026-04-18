d034de72a3b26b6d15a302f46a796f41a922c94f
rafavicruz <rafaelvicruz@gmail.com>
2026-04-18 14:35:17 -0300

    index

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
-import '../styles/main.css'
+import '../src/styles/globals.css'
+import '../src/styles/main.css'
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
